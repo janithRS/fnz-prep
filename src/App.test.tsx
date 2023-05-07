f6e2f2cb7e43562d58bf106c7a6a5328cfba846e
janithrs <janithravindu@gmail.com>
2023-05-07 22:15:05 +0530

    update test coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import { describe, expect, it } from "vitest";
 import App from "./App";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("App", async () => {
   it("should render", () => {
     render(<App />);
     expect(screen.getByText("Vite + React")).toBeInTheDocument();
+  });
+
+  it("should increment count", () => {
+    render(<App />);
+    fireEvent.click(screen.getByText("count is 0"));
+    expect(screen.getByText("count is 1")).toBeInTheDocument();
   });
 });
